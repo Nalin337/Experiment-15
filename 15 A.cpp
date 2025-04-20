f533f218ff57ef62eae7fbcd9836440426570488
Nalin337 <nalinsrivastava.nks@gmail.com>
2025-04-20 12:07:29 +0530

    Add files via upload

---
+++ 15 A.cpp
@@ -0,0 +1,23 @@
+//NALIN KUMAR SRIVASTAVA
+//23070123157
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * fact(n - 1);  // Recursive case
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter number: ";
+    cin >> number;
+
+    int result = fact(number);
+    cout << number << "! = " << result << endl;
+
+    return 0;
+}
---
+++ 15 B.cpp
@@ -0,0 +1,23 @@
+//NALIN KUMAR SRIVASTAVA
+//23070123157
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n == 0) {
+        return 0;
+    } else {
+        return n + sum(n - 1);
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter number: ";
+    cin >> number;
+
+    int result = sum(number);
+    cout << "Sum = " << result << endl;
+
+    return 0;
+}
